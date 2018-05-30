da0c181d9ce9ac43a42b18dfb76f2d0cae39dcc6
adinoegroho <37955862+adinoegroho@users.noreply.github.com>
2018-05-30 21:51:45 +0700

    Add files via upload

---
+++ assets/part/footer.php
@@ -0,0 +1,43 @@
+<!-- html script -->
+<!DOCTYPE html> 
+<html lang="en" id="footer">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title></title>
+
+    <!-- Bootstrap -->
+    <link href="../../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <link href="../../css/footer-style.css" rel="stylesheet">
+    
+  </head>
+  <body>
+
+    <!-- Awal Footer -->
+    <footer>
+      <div class="container">
+        <dir class="rows">
+          <p class="text-center">&copy; 2018 Auto E Grande.</p>
+        </dir>        
+      </div>
+    </footer>
+    <!-- Akhir Footer -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="../../js/jquery-3.3.1.min.js"></script>
+    <script src="../../js/jquery.easing.1.3.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../../js/bootstrap.min.js"></script>
+
+    <script src=""></script>
+  </body>
+</html>
