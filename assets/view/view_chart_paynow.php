847bc6fe4e95573a98089f6673ce46f527bd12ec
adinoegroho <37955862+adinoegroho@users.noreply.github.com>
2018-06-07 19:19:25 +0700

    Add files via upload

---
+++ assets/view/view_chart_paynow.php
@@ -0,0 +1,28 @@
+
+    <!-- Mengisi ruang dibawah Navbar -->
+
+    <div class="space-navbar"></div>
+
+    <!-- Mengisi ruang dibawah Navbar -->
+
+    <!-- Chart Pay Now -->
+
+    <section id="chartPayNow">
+      <div class="row">
+        <div class="col-sm-10 col-sm-offset-1">
+
+          <div class="col-sm-8 col-sm-offset-2 text-center">
+            <h3>ACCOUNT NUMBER : XXXXXXXXX</h3>
+            <h3>In The Name of : AUTO E GRANDE</h3>
+          </div>
+          <hr>
+
+          <div class="col-sm-8 col-sm-offset-2 text-center">
+            
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <!-- Chart Pay Now -->
---
+++ assets/view/view_list.php
@@ -0,0 +1,121 @@
+
+<!-- Mengisi ruang dibawah Navbar -->
+
+<div class="space-navbar"></div>
+
+<!-- Mengisi ruang dibawah Navbar -->
+
+<!-- Car List -->
+
+<section id="carList">
+  <div class="row">
+    <div class="col-sm-11 col-sm-offset-1">
+
+      <!-- Item List -->
+
+      <div class="col-sm-3 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/laferrari.png" alt="laferrari">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-sm-offset-1 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/lexusrx.png" alt="lexusrx">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-sm-offset-1 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/jaguarftype.png" alt="jaguarftype">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/laferrari.png" alt="laferrari">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-sm-offset-1 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/lexusrx.png" alt="lexusrx">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-sm-offset-1 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/jaguarftype.png" alt="jaguarftype">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/laferrari.png" alt="laferrari">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-sm-offset-1 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/lexusrx.png" alt="lexusrx">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <div class="col-sm-3 col-sm-offset-1 itemList text-center">
+        <h4>Car Name</h4>
+        <img src="assets/images/jumbotron/jaguarftype.png" alt="jaguarftype">
+        <div class="col-sm-6 text-left">
+          <button type="button" class="btn btn-default">Detail</button>
+        </div>
+        <div class="col-sm-6 text-right">
+          <button type="button" class="btn btn-default">Buy Now</button>
+        </div>
+      </div>
+
+      <!-- Item List -->
+      
+    </div>
+  </div>
+</section>
+
+<!-- Car List -->
