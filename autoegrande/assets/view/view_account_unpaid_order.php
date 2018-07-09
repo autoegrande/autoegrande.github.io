8a8e6d8646fc2e3f61ea635b92a9b619847967c8
Bimo Firmansyah <37612234+bimofirmansyah24@users.noreply.github.com>
2018-07-09 10:40:05 +0700

    q

--- autoegrande/assets/view/view_account_unpaid_order.php
+++
@@ -1,9 +0,0 @@
-<!-- Unpaid Order -->
-
-<div class="col-sm-8">
-	<div class="col-sm-8 col-sm-offset-2">
-		
-	</div>
-</div>
-
-<!-- Unpaid Order -->
--- autoegrande/assets/view/view_footer.php
+++
@@ -1,9 +0,0 @@
-<!-- Awal Footer -->
-<footer>
-<div class="container footer-website">
-  <div class="rows">
-    <p class="text-center">&copy; 2018 Auto E Grande.</p>
-  </div>        
-</div>
-</footer>
-<!-- Akhir Footer -->
