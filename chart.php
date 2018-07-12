69558902a562018afa86e93b374b8e48a96d903d
Bimo Firmansyah <37612234+bimofirmansyah24@users.noreply.github.com>
2018-07-12 20:28:10 +0700

    q

--- chart.php
+++ chart.php
@@ -4,11 +4,30 @@ include "assets/controller/controller.php";
 
 $main = new controller();
 // Kondisi untuk menampilkan halaman web yang diminta
-if(isset($_POST['signinsubmit']))
+if(isset($_POST['loginsubmit']))
 {
-
+	// Kondisi untuk mengakses halaman Login
+  $main->Login();
+}
+else if(isset($_POST['navbarbtn']))
+{
+	switch ($_POST['navbarbtn']) 
+	{
+		case 'account':
+			$main->MovePage($_POST['navbarbtn']);
+			break;
+		case 'chart':
+			$main->MovePage($_POST['navbarbtn']);
+			break;
+		case 'carlist':
+			$main->MovePage($_POST['navbarbtn']);
+			break;
+		case 'rent':
+			$main->MovePage($_POST['navbarbtn']);
+			break;
+	}
 }
 else
 {
-    $main->Chart();
+  $main->Chart();
 }
